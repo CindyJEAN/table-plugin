e20034dcf35a194822413df6aadac242634a38f2
Cindy JEAN <cindy.jean83@gmail.com>
2022-06-21 16:38:33 +0200

    feat: sort table (beginning)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,56 @@
-import React from "react";
-// import "./styles.css";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 export const TablePlugin = ({ data, headCells }) => {
+  const [tableData, setTableData] = useState([]);
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortField, setSortField] = useState("");
+
+  function handleSortRequest(headCell) {
+    const order =
+      headCell === sortField && sortOrder === "asc" ? "desc" : "asc";
+    setSortOrder(order);
+    setSortField(headCell);
+  }
+
+  function sortArray(data, sortOrder, sortField) {
+    const sortedArray = data.sort((a, b) => {
+      return (
+        a[sortField].localeCompare(b[sortField]) *
+        (sortOrder === "asc" ? 1 : -1)
+      );
+    });
+    return sortedArray;
+  }
+
+  useEffect(() => {
+    setTableData(data);
+  }, []);
+
+  useEffect(() => {
+    const sortedArray = sortArray(tableData, sortOrder, sortField);
+    setTableData(sortedArray);
+  }, [sortOrder, sortField]);
+
+
   return (
     <div className={styles.component}>
       <table>
         <thead>
           <tr>
             {headCells.map((headCell, index) => (
-              <th key={index}>{headCell.label}</th>
+              <th key={index} onClick={() => handleSortRequest(headCell.data)}>
+                {headCell.label}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.map((employee, employeeIndex) => (
-            <tr key={employeeIndex}>
-              {Object.keys(employee).map((cell, cellIndex) => (
-                <td key={cellIndex}>{employee[cell]}</td>
-              ))}
+          {tableData?.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {headCells.map(({ data: headCell }) => {
+                return <td key={headCell}>{row[headCell] || ""}</td>;
+              })}
             </tr>
           ))}
         </tbody>
