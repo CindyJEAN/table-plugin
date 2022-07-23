ba00f8347de75f0cae979c111e8357dbea568a07
Cindy JEAN <cindy.jean83@gmail.com>
2022-07-23 21:14:25 +0200

    fix: test import arrow svg

--- src/components/tableHeadCell.js
+++ src/components/tableHeadCell.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "../styles.module.css";
 import { getSortArrowClassName } from "../utils/dataManager";
-// import icon from "../assets/arrow_down.svg";
-const icon = `${require("./arrow_down.svg")}`;
+import icon from "../assets/arrow_down.svg";
+// import icon from "./assets/arrow_down.svg";
+// const icon = `${require("./arrow_down.svg")}`;
 
 /**
  * @description Displays a headCell with a sort arrow
